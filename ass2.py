2129cfff2fa563e83ee135ea4e50bde0a5beeef8
SaHiL GoYaL <sahilnice490@gmail.com>
2018-06-25 14:37:28 -0400

    all done

---
+++ ass2.py
@@ -0,0 +1,42 @@
+# Q.1- Print anything you want on screen.
+#Ans:-
+print("My name is Sahil Goyal")
+
+#Q.2- Join two strings using '+'.
+#E.g.-"Acad"+"View”
+#Ans-
+x="Sahil"
+y="Goyal"
+print(x+y)
+
+#Q.3- Take the input of 3 variables x, y and z . Print their values on screen. 
+#Ans:-
+x=10
+y=20
+z=30
+print(x,y,z,)
+
+#Q.4- Print “Let’s get started” on screen.
+#Ans-
+print("Let’s get started") 
+
+#Q.5- Print the following values using placeholders.
+#s=”Acadview”
+#course=”Python”
+#fees=5000
+#Ans-
+s='"Acadview"\n'
+course="Python\n"
+fees=5000
+print('s=%s course=%s fees=%d'%(s,course,fees))
+
+#Q.6- Let’s do some interesting exercise:
+#name=”Tony Stark”
+#salary=1000000
+#print(‘%s’’%d’)%(____ ,____) 
+#Ans:-
+name="Tony Stark\n"
+salary=1000000
+print('Name=%s Fees=%d'%(name,salary) )
+
+ 
